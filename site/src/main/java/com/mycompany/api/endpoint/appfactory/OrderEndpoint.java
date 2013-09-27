6655f04d4a6fec0e66e4524a3c07427acf197402
William Li <wli@objectivasoftware.com>
2013-09-27 16:09:09 +0800

    create order endpoint class

---
+++ site/src/main/java/com/mycompany/api/endpoint/appfactory/OrderEndpoint.java
@@ -0,0 +1,27 @@
+package com.mycompany.api.endpoint.appfactory;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wli
+ * Date: 9/27/13
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Scope("singleton")
+@Path("/orders")
+@Produces(value = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes(value = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class OrderEndpoint {
+
+
+
+}
